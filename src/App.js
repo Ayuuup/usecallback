413712dd51ea0842fd3c421006b5920365456f60
ayuuup <babakhouyaayoub@gmail.com>
2025-04-19 00:12:11 +0100

    final

--- src/App.js
+++ src/App.js
@@ -4,23 +4,13 @@ import P from "./P";
 import B from "./B";
 function App() {
 
-  const [input,setInput] = useState('')
-  const [num1] = useState(4)
-  const [num2] = useState(5)
+
   
   const [o,setObj] = useState([1,2])
 
   const handleClick = useCallback(()=>console.log(o.length),[o.length])
 
 
-
-
-  //memoize the sum function so that it wouldnt be recreated upon rerenders
-  const sum = useCallback(()=>num1+num2,[num1,num2])
-
-  const sum2= ()=>num1+num2
-
-  useEffect(()=>console.log(sum()),[sum])
   return (
   <div>
     <Input val={input} setVal={setInput}></Input>
